9188d962e45eabcb68b90f610eb4de2424875450
MihailAlexeev <84586014+MihailAlexeev@users.noreply.github.com>
2021-06-13 14:59:02 +0300

    Add files via upload

---
+++ QG.pro
@@ -0,0 +1,51 @@
+QT       += core gui xml network printsupport
+
+greaterThan(QT_MAJOR_VERSION, 4): QT += widgets
+
+CONFIG += c++17
+
+# You can make your code fail to compile if it uses deprecated APIs.
+# In order to do so, uncomment the following line.
+#DEFINES += QT_DISABLE_DEPRECATED_BEFORE=0x060000    # disables all the APIs deprecated before Qt 6.0.0
+
+SOURCES += \
+    main.cpp \
+    mainwindow.cpp \
+    mcl.cpp
+
+HEADERS += \
+    mainwindow.h \
+    mcl.h
+
+FORMS += \
+    mainwindow.ui
+
+# Default rules for deployment.
+qnx: target.path = /tmp/$${TARGET}/bin
+else: unix:!android: target.path = /opt/$${TARGET}/bin
+!isEmpty(target.path): INSTALLS += target
+
+unix:!macx: LIBS += -L$$PWD/../apps_2/lib/ -lqgis_app
+
+INCLUDEPATH += $$PWD/../apps_2/include/qgis
+DEPENDPATH += $$PWD/../apps_2/include/qgis
+
+unix:!macx: LIBS += -L$$PWD/../apps_2/lib/ -lqgis_core
+
+INCLUDEPATH += $$PWD/../apps_2/include/qgis
+DEPENDPATH += $$PWD/../apps_2/include/qgis
+
+unix:!macx: LIBS += -L$$PWD/../apps_2/lib/ -lqgis_gui
+
+INCLUDEPATH += $$PWD/../apps_2/include/qgis
+DEPENDPATH += $$PWD/../apps_2/include/qgis
+
+unix:!macx: LIBS += -L$$PWD/../apps_2/lib/ -lqgis_native
+
+INCLUDEPATH += $$PWD/../apps_2/include/qgis
+DEPENDPATH += $$PWD/../apps_2/include/qgis
+
+unix:!macx: LIBS += -L$$PWD/../../../usr/lib/ -lgdal
+
+INCLUDEPATH += $$PWD/../../../usr/include/gdal
+DEPENDPATH += $$PWD/../../../usr/include/gdal
---
+++ mainwindow.h
@@ -0,0 +1,76 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QComboBox>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QDockWidget>
+#include "mcl.h"
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+
+
+
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void clickedbutton();
+    void choosenoption(int index);
+    void clickedbutton2();
+    void lat_input(QString str);
+    void lon_input(QString str);
+    void trj_input(QString str);
+    void type_option(int index);
+
+private:
+        QgsVectorLayer* type1_layer = new QgsVectorLayer("Point", "temp_points","memory");
+        QgsVectorLayer* type2_layer = new QgsVectorLayer("Point", "temp_points","memory");
+        QgsVectorLayer* type3_layer = new QgsVectorLayer("Point", "temp_points","memory");
+        QgsVectorLayer* linea = new QgsVectorLayer("LineString?crs=epsg:4326&field=id:integer&index=yes","Linea","memory");
+        QgsVectorLayerSimpleLabeling* simpleL;
+
+
+        enum TYPES {
+            type1,
+            type2,
+            type3,
+        };
+
+
+        int param = 0;
+        std::vector<QgsPoint> xys;
+        std::vector<std::pair<int, TYPES>> trj_and_type;
+
+        std::map<int, QVector<QgsPoint>> trj_to_point;
+
+
+        double curr_x;
+        double curr_y;
+        int curr_trj;
+        TYPES curr_type = TYPES::type1;
+
+        Ui::MainWindow *ui;
+
+        QPushButton* button;
+        QPushButton* button2;
+
+        QLineEdit* lat;
+        QLineEdit* lon;
+        QLineEdit* trj;
+        QComboBox* type;
+
+        QComboBox* box;
+        MCL canv = MCL(this);
+
+};
+#endif // MAINWINDOW_H
