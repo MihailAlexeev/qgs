55d695313d9396a9f81fbc7047195c683a3b979b
MihailAlexeev <84586014+MihailAlexeev@users.noreply.github.com>
2021-07-28 21:49:35 +0300

    Add files via upload

--- relativedistancewidget.cpp
+++ relativedistancewidget.cpp
@@ -3,11 +3,11 @@
 RelativeDistanceWidget::RelativeDistanceWidget(QWidget *parent) : QWidget(parent){
     QVBoxLayout* lay = new QVBoxLayout;
     label = new QLabel;
-    label->setText("SHIP COORDS: ");
+    label->setText("Координаты корабля: ");
     table = new QTableWidget;
     table->setRowCount(1);
     table->setColumnCount(3);
-    table->setHorizontalHeaderLabels({"Object ID", "Relative distance", "Coordinates"});
+    table->setHorizontalHeaderLabels({"ID Объекта", "Относительное расстояние", "Координаты"});
     lay->addWidget(label);
     lay->addWidget(table);
     this->setLayout(lay);
@@ -19,9 +19,11 @@ RelativeDistanceWidget::~RelativeDistanceWidget() {
 }
 
 void RelativeDistanceWidget::getShipCoords(const std::vector<TrajData> &trajectories) {
+    table->setRowCount(trajectories.size());
     for (const auto& i : trajectories) {
         if (i.type == SHIP) {
             ship_id = i.numTr;
+            table->hideRow(ship_id);
             for (const auto& e : i.coords) {
                 geod_ship.latitude = e.latitude;
                 geod_ship.longitude = e.longitude;
@@ -33,15 +35,15 @@ void RelativeDistanceWidget::getShipCoords(const std::vector<TrajData> &trajecto
     x_ship = geoc_ship.x_;
     y_ship = geoc_ship.y_;
     z_ship = geoc_ship.z_;
-    label->setText("SHIP COORDS: " + QString::number(geod_ship.latitude) + ", " + QString::number(geod_ship.longitude));
+    label->setText("Координаты корабля: " + QString::number(geod_ship.latitude) + ", " + QString::number(geod_ship.longitude));
 }
 
 void RelativeDistanceWidget::getOtherObjsCoords(int id, double latitude, double longitude, double altitude) {
     if (id != ship_id) {
-//        table->setItem(id + 2, 0, new QTableWidgetItem(QString::number(id)));
-//        double dist = calculateRDistance(latitude, longitude, altitude);
-//        table->setItem(id + 2, 1, new QTableWidgetItem(QString::number(dist)));
-//        table->setItem(id + 2, 2, new QTableWidgetItem(QString::number(latitude) + " " + QString::number(longitude) + " " + QString::number(altitude)));
+        table->setItem(id, 0, new QTableWidgetItem(QString::number(id)));
+        double dist = calculateRDistance(latitude, longitude, altitude);
+        table->setItem(id, 1, new QTableWidgetItem(QString::number(dist)));
+        table->setItem(id, 2, new QTableWidgetItem(QString::number(latitude) + " " + QString::number(longitude) + " " + QString::number(altitude)));
     }
 }
 
